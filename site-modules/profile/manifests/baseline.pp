930092725d889fe54798d1cc5988e8d4756b36ea
Andrew Jones <andrew.jones@puppet.com>
2025-01-24 16:15:05 +0000

    PANIC

--- site-modules/profile/manifests/baseline.pp
+++ site-modules/profile/manifests/baseline.pp
@@ -3,7 +3,7 @@
 #   An array of the NTP sources to use.
 #
 class profile::baseline (
-  Array[Stdlib::Fqdn] $source
+  Array[Stdlib::Blablabla] $source
 ) {
   class { 'ntp':
     servers => $source,

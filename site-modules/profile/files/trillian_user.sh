bba461c12d098cc01cd2431653145302d27d65c3
Andrew Jones <andrew.jones@puppet.com>
2025-01-26 13:19:41 +0000

    PANIC

--- site-modules/profile/files/trillian_user.sh
+++ site-modules/profile/files/trillian_user.sh
@@ -3,7 +3,3 @@
 sudo groupadd earth
 
 sudo useradd -m -d /home/trillian -c "Tricia McMillan" -G earth trillain
-sudo chown trillain:earth /home/trillian
-sudo chmod 640 /home/trillian
-
-echo "User 'trillain' created successfully."

563c89e954c0afbc103845a0f45c842252e2f5d6
Andrew Jones <andrew.jones@puppet.com>
2025-01-27 18:16:51 +0000

    PANIC

--- site-modules/profile/manifests/webserver.pp
+++ site-modules/profile/manifests/webserver.pp
@@ -1,9 +1,5 @@
 #
 class profile::webserver {
-  apache::vhost { 'user.example.com':
-    port          => 80,
-    docroot       => '/var/www/user',
-    docroot_owner => 'www-data',
-   docroot_group => 'www-data',
+  class {'webapp':
   }
 }
---
+++ site-modules/webapp/manifests/init.pp
@@ -0,0 +1,15 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include webapp
+class webapp {
+  $port = '80',
+
+  file { /etc/webapp.conf :
+    ensure => file,
+    content => "port: ${port}",
+  }
+
+}

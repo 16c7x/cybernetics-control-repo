16790426b1dfc18da63b856dcad41074c8e3abd1
Andrew Jones <andrew.jones@puppet.com>
2025-01-27 17:48:45 +0000

    PANIC

--- site-modules/profile/manifests/webserver.pp
+++ site-modules/profile/manifests/webserver.pp
@@ -1,7 +1,9 @@
 #
 class profile::webserver {
-  apache::vhost { 'localhost':
-    port    => 80,
-    docroot => '/var/www/vhost',
-  }  
+  apache::vhost { 'user.example.com':
+    port          => 80,
+    docroot       => '/var/www/user',
+    docroot_owner => 'www-data',
+   docroot_group => 'www-data',
+  }
 }

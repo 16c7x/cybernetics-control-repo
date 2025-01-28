8064700872aff4d2a316a11b3b8799c044cbf29c
Andrew Jones <andrew.jones@puppet.com>
2025-01-28 21:26:36 +0000

    PANIC

--- site-modules/profile/manifests/db_user.pp
+++ site-modules/profile/manifests/db_user.pp
@@ -1,16 +1,24 @@
 #
-class profile::db_user {
-  $password = 'password567'
+class profile::db_user (
+    String $private_key
+){
   user { 'postgres':
     ensure     => 'present',
     gid        => 'dba',
     home       => '/home/postgres',
     managehome => 'true',
-    password   => $password,
     comment    => $password,
   }
 
   group { 'dba':
     ensure => 'present',
+  }
+
+  file { '/home/postgres/key':
+    ensure => file,
+    owner  => postgres,
+    group  => dba,
+    mode   => '0600'
+    content => $private_key,
   }
 }

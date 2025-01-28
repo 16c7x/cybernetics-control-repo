4b25f98d88cbdb49d3e922ccb9717ae11eab16e1
Andrew Jones <andrew.jones@puppet.com>
2025-01-28 21:14:28 +0000

    PANIC

--- site-modules/profile/manifests/db_user.pp
+++ site-modules/profile/manifests/db_user.pp
@@ -3,7 +3,7 @@ class profile::db_user (
   String $password
   #password= Sensitive('password123')
 ){
-  $password = Sensitive('password123')
+  #$password = Sensitive('password123')
   user { 'postgres':
     ensure     => 'present',
     gid        => 'dba',

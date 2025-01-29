9ca299b63ba2f390aeb90e48417016b144d1f910
Andrew Jones <andrew.jones@puppet.com>
2025-01-29 20:04:17 +0000

    PANIC

--- site-modules/profile/manifests/db_user.pp
+++ site-modules/profile/manifests/db_user.pp
@@ -1,9 +1,7 @@
 #
 class profile::db_user (
-    #String $private_key
-    Sensitive[String[1]] $mykey
+    Sensitive[String[1]] $password
 ){
-  $password=Sensitive('pass456')
   user { 'postgres':
     ensure     => 'present',
     gid        => 'dba',
@@ -16,42 +14,4 @@ class profile::db_user (
   group { 'dba':
     ensure => 'present',
   }
-
-  file { '/home/postgres/key':
-    ensure  => file,
-    owner   => postgres,
-    group   => dba,
-    mode    => '0600',
-  }
-
-  file_line { '/home/postgres/key':
-      ensure => present,
-      path   => '/home/postgres/key',
-      line   => "key: ${mykey}",
-      match  => '^key:',
-  }
-
-  notify { 'secret message':
-    message => $mykey,
-    }
-
-  ssh_authorized_key { 'nick@magpie.example.com':
-    ensure => present,
-    user   => 'postgres',
-    type   => 'ssh-rsa',
-    key    => 'AAAAB3Nza[...]qXfdaQ==',
-  }
-
-  $user_hash = {
-    'userdata' => {
-      'password' => unwrap($mykey),
-    }
-  }
-  
-  file { '/home/postgres/user.yaml':
-    ensure  => file,
-    mode    => '0640',
-    content => to_yaml($user_hash).node_encrypt::secret,
-  }
-
 }

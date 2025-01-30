e18f64a0081d07b76ed88eab1157862d382d0500
Andrew Jones <andrew.jones@puppet.com>
2025-01-30 11:19:29 +0000

    PANIC

--- site-modules/profile/manifests/db_user.pp
+++ site-modules/profile/manifests/db_user.pp
@@ -18,7 +18,8 @@ class profile::db_user (
 
   file {'/home/postgres/keyfile':
     ensure => file,
-    content => Deferred("node_decrypt", [$password]),
+    #content => Deferred("node_decrypt", [$password]),
+    content => 'test123'.node_encrypt::secret
   }
 }
 
